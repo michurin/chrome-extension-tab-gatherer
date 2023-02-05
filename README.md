8511706d6a9487e3fc76a9862d192bd9ee3d8fd2
Alexey Michurin <a.michurin@gmail.com>
2023-02-05 12:08:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tab-gatherer
+
+Extension groups tabs for you automatically.
+Groups are based on domains and trying to take into account your current groups of tabs.
+
+### TODO
+
+- [must have] Icons in manifest
+- `homepage_url` in manifest
+- [must have] Publish (while you can try it in deveoper mode)
+- Extending logic of grouping (?)
+- Saving state (?) saving preferences (domains? groups names? colors?)
---
+++ background.js
@@ -0,0 +1,42 @@
+async function move(tabId, tabUpdate, activeInfo) {
+  if (!tabUpdate.url) {
+    return;
+  }
+  const url = new URL(tabUpdate.url);
+  if (!url.protocol.startsWith('http')) {
+    return;
+  }
+  const { host } = url;
+  console.log(tabId, tabUpdate, activeInfo);
+  const allInWindow = await chrome.tabs.query({ windowId: activeInfo.windowId });
+  const locator = {};
+  const similar = [tabId];
+  allInWindow.forEach((x) => {
+    if (x.id === tabId) { // skip itself
+      return;
+    }
+    if ((new URL(x.url)).host === host) {
+      if (x.groupId === -1) {
+        similar.push(x.id);
+      } else {
+        locator[x.groupId] = (locator[x.groupId] || 0) + 1;
+      }
+    }
+  });
+  let targetGroup;
+  let targetScore = 0;
+  Object.keys(locator).forEach((k) => { // TODO pretty unstable way to figure out maximum score
+    const v = locator[k];
+    if (v > targetScore) {
+      targetScore = v;
+      targetGroup = +k; // as far as keys are always strings we are to cast they back to ints
+    }
+  });
+  if (targetGroup !== undefined) {
+    await chrome.tabs.group({ groupId: targetGroup, tabIds: [tabId] });
+  } else if (similar.length > 1) {
+    await chrome.tabs.group({ tabIds: similar }); // TODO setup group?
+  }
+}
+
+chrome.tabs.onUpdated.addListener(move);
